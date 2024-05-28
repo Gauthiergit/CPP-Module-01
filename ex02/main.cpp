8be483d7eb443a2b1a28ca5ca4671af81a93ddab
Gauthier Peyre <gpeyre@c3r2p8.42perpignan.fr>
2024-05-28 17:54:51 +0200

    ex02

---
+++ ex02/main.cpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gpeyre <gpeyre@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/28 17:32:03 by gpeyre            #+#    #+#             */
+/*   Updated: 2024/05/28 17:52:16 by gpeyre           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <string>
+#include <iostream>
+
+int	main()
+{
+	std::string brain = "HI THIS IS BRAIN";
+	std::string* stringPTR = &brain;
+	std::string& stringREF = brain;
+
+	std::cout << "The memory address of brain: " << &brain << std::endl;
+	std::cout << "The memory address held by stringPTR: " << stringPTR << std::endl;
+	std::cout << "The memory address held by stringREF: " << &stringREF << std::endl;
+	std::cout << std::endl;
+	std::cout << "The value of the brain: " << brain << std::endl;
+	std::cout << "The value pointed to by stringPTR: " << *stringPTR << std::endl;
+	std::cout << "The value pointed to by stringPTR: " << stringREF << std::endl;
+}
