de6f38ae403708d8ed1e4ad4178e1eda502e2012
Gauthier Peyre <gpeyre@c3r2p8.42perpignan.fr>
2024-05-30 11:41:24 +0200

    ex04

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -6,7 +6,7 @@
 /*   By: gpeyre <gpeyre@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/29 11:20:03 by gpeyre            #+#    #+#             */
-/*   Updated: 2024/05/29 15:29:37 by gpeyre           ###   ########.fr       */
+/*   Updated: 2024/05/30 11:37:07 by gpeyre           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,14 +18,10 @@ int	main(int argc, char **argv)
 {
 	if (argc != 4)
 	{
-		std::cerr << "Use: ./replace <infile.txt> <string to replace> <replacement string>" << std::endl;
+		std::cerr << "Use: ./replace <infile.txt> <string to find> <string to replace>" << std::endl;
 		return (1);
 	}
 	
-	std::string new_string;
-	std::string to_find;
-	std::string to_replace;
-
 	std::string infile_name = argv[1];
 	std::ifstream infile(infile_name.c_str());
 	if (!infile.is_open())
@@ -34,7 +30,7 @@ int	main(int argc, char **argv)
 		return (1);
 	}
 
-	std::string outfile_name = "outfile.txt";
+	std::string outfile_name = "file.replace";
 	std::ofstream outfile(outfile_name.c_str());
 	if (!outfile.is_open())
 	{
@@ -42,12 +38,35 @@ int	main(int argc, char **argv)
 		return (1);
 	}
 	
-	to_find = argv[2];
-	to_replace = argv[3];
-
+	std::string to_find = argv[2];
+	std::string to_replace = argv[3];
+	std::string new_string;
+	std::size_t pos;
+    std::size_t prev_pos;
 	std::string line;
 	while (std::getline(infile, line))
 	{
-		
+		pos = 0;
+		prev_pos = 0;
+		new_string = "";
+		if ((pos = line.find(to_find, pos)) != std::string::npos)
+		{
+			while ((pos = line.find(to_find, pos)) != std::string::npos)
+			{
+				new_string.append(line, prev_pos, pos - prev_pos);
+				new_string.append(to_replace);
+				pos += to_find.length();
+				prev_pos = pos;
+			}
+			if (pos < line.length())
+				new_string.append(line, prev_pos, line.length() - prev_pos);
+			outfile << new_string << std::endl;
+		}
+		else
+			outfile << line << std::endl;
 	}
+
+	infile.close();
+	outfile.close();
+	return (0);
 }
